806129504a5ccc0a5d5c07f536421725e1ba8a9e
Kyle Murphy <noevidenz@gmail.com>
2019-09-28 23:11:44 +1000

    Moved styles into _sass

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     mode: process.env.NODE_ENV,
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, '_sass')
     },
 
     plugins: [
